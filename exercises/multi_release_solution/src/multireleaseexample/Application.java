bb4505523b48e1873653d1d580598161b8c1e2e6
Michael Inden <michael_inden@hotmail.com>
2021-04-19 15:57:57 +0200

    Java Update Workshop Initialer Stand

---
+++ exercises/multi_release_solution/src/multireleaseexample/Application.java
@@ -0,0 +1,10 @@
+package multireleaseexample;
+
+public class Application 
+{
+    public static void main(final String[] args)
+    {
+    	final Generator gen = new Generator();
+        System.out.println("Generated strings: " + gen.createStrings());
+    }
+}
---
+++ exercises/multi_release_solution/srcjdk9/multireleaseexample/Generator.java
@@ -0,0 +1,12 @@
+package multireleaseexample;
+
+import java.util.List;
+import java.util.Collection;
+
+public class Generator 
+{
+   public Collection<String> createStrings() 
+   {
+      return List.of("New", "JDK 9", "Collection", "Factory", "Methods");
+   }
+}
